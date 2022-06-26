dee9c2288d1ffd274dc9280766aa10fb76cf1dd1
Jae Deok <Yesman3>
2022-06-26 15:03:07 +0900

    부트스트랩 사용하기, 이미지 넣는 법

--- shop/src/App.js
+++ shop/src/App.js
@@ -4,21 +4,49 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import Button from 'react-bootstrap/Button';
+
+
 
 function App() {
   return (
     <div className="App">
-       <Navbar bg="dark" variant="dark">
+      <Navbar bg="primary" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">jelly deok</Navbar.Brand>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">용품</Nav.Link>
-            <Nav.Link href="#pricing">캐릭터</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      
+    
+      
+      {/* <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+            </div>
+            <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+            </div>
+            <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+            </div>
+          </div>
+        </div>  */}
+
     </div>
+    
   );
 }
 
