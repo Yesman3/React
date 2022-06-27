59e55f4db770fa9ffe03f39f1d840a85ed18b097
Jae Deok <Yesman3>
2022-06-27 22:20:43 +0900

    import, export 쓰기 /// 데이터 바인딩 활용하기

--- shop/src/App.js
+++ shop/src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
+import {useState} from 'react';
+import data from './data.js';
 
 
 
 function App() {
+
+  let [ shoes ] = useState(data);
+  let [글제목,글제목변경] = useState(0,0,0);
+  
+
   return (
     <div className="App">
+      
       <Navbar bg="primary" variant="dark">
         <Container>
           <Navbar.Brand href="#home">Navbar</Navbar.Brand>
@@ -25,29 +32,29 @@ function App() {
       
     
       
-      {/* <div className="container">
+      <div className="container">
         <div className="row">
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-            </div>
-            <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-            </div>
-            <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
-            <h4>상품명</h4>
-            <p>상품설명</p>
-            </div>
+
+            {
+              [1,2,3].map(function (a, i){
+                return(
+                  <div className="col-md-4" key={i}>
+                  <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+                  <h4>{shoes[i].title}</h4>
+                  <p>{shoes[i].price}</p>
+                  </div>
+                )
+              })
+            }
+
+
           </div>
-        </div>  */}
+        </div> 
 
     </div>
     
   );
 }
+
 
 export default App;
